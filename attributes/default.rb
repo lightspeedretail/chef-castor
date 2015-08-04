4948b624e73c3d1c282b3503a37a8b4490443648
Al Lefebvre <alain.lefebvre@lightspeedpos.com>
2015-08-03 21:32:51 -0400

    Updated version attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['castor']['version'] = '0.4.3'
+default['castor']['version'] = '0.4.4'
 default['castor']['user'] = 'castor'
 default['castor']['group'] = 'castor'
 default['castor']['base_dir'] = '/opt/castor'
