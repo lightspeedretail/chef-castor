55dfd09f96b1a0a168e0781092ba30404ddf848f
Jean-Francois Theroux <me@failshell.io>
2015-08-12 13:49:47 -0400

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'retailops@lightspeedpos.com'
 license          'MIT'
 description      'Installs/Configures castor'
 long_description 'Installs/Configures castor'
-version          '0.4.4'
+version          '0.4.5'
 
 depends          'python', '>= 0.9.0'
 depends          'logrotate', '>= 1.7.0'
