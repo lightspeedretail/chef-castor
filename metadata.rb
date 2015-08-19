e364b2c6f8ef16c52a36df08875510190652a245
Jean-Francois Theroux <me@failshell.io>
2015-08-19 11:46:58 -0400

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'retailops@lightspeedpos.com'
 license          'MIT'
 description      'Installs/Configures castor'
 long_description 'Installs/Configures castor'
-version          '0.4.7'
+version          '0.4.8'
 
 depends          'python', '>= 0.9.0'
 depends          'logrotate', '>= 1.7.0'
